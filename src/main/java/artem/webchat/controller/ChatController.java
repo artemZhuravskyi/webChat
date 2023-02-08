6a11f3d1b8b03c0805f7a66787e2e00ba7d86b25
artemZhuravskyi <artem.zhuravskyi22@gmail.com>
2023-02-08 11:46:50 +0200

    Initial Commit

---
+++ src/main/java/artem/webchat/controller/ChatController.java
@@ -0,0 +1,23 @@
+package artem.webchat.controller;
+
+import artem.webchat.model.Chat;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/chat.register")
+    @SendTo("/topic/public")
+    public Chat register(@Payload Chat chat) {
+        return chat;
+    }
+
+    @MessageMapping("/chat.send")
+    @SendTo("/topic/public")
+    public Chat sendMessage(@Payload Chat chat) {
+        return chat;
+    }
+}
---
+++ src/main/java/artem/webchat/model/Chat.java
@@ -0,0 +1,15 @@
+package artem.webchat.model;
+
+import lombok.Data;
+
+@Data
+public class Chat {
+    private String content;
+    private String sender;
+    private MessageType type;
+
+    public enum MessageType {
+        CHAT, JOIN
+    }
+
+}
